6eb9f8ea9da0f6fdce7bb1c9ab907c2250925d02
AshaDevi18 <ashakara28@gmail.com>
2023-05-17 17:14:51 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import pengguna.Mhs;
+
+class Main {
+    public static void main(String[] args) {
+
+        Mhs mhs = new Mhs("Ini_Username", "Ini_Password", "Asha Adonia", "G.111.19.0018", 8);
+
+        // mhs.setNama("Asha Adonia");
+        // mhs.setNim("G.111.19.0018");
+        // mhs.setSmt(8);
+
+        System.out.println("Nama Mhs : " + mhs.getNama());
+        System.out.println("Nim Mhs : " + mhs.getNim());
+        System.out.println("Smt Mhs : " + mhs.getSmt());
+        System.out.println("Username : " + mhs.getUsername());
+        System.out.println("Password : " + mhs.getPassword());
+
+    }
+}
